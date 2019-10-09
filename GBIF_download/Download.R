3508566308ebe85fbcb5d5d2fcccd12ad115d30b
Lyder Bøe Iversen <lyderi@ntnu.no>
2019-10-09 12:55:09 +0200

    Fixed Download.R to work on remote computer and added 'coffeebreak version' (prefered version). Need to test on other computers

--- GBIF_download/Download.R
+++ GBIF_download/Download.R
@@ -26,6 +26,10 @@ options(gbif_user=rstudioapi::askForPassword("my gbif username"))
 options(gbif_email=rstudioapi::askForPassword("my registred gbif e-mail"))
 options(gbif_pwd=rstudioapi::askForPassword("my gbif password"))
 
+options(gbif_user = "lyderiversen")
+options(gbif_email = "lyder.iversen95@hotmail.com")
+options(gbif_pwd="Retsam144")
+
 #-------------------------------------------------------------------------------
 # Set search parameters and get download KEY
 #-------------------------------------------------------------------------------
@@ -65,11 +69,32 @@ download.file(url=paste("http://api.gbif.org/v1/occurrence/download/request/",
 
 # or....
 # coffebreak version (see https://github.com/GBIF-Europe/nordic_oikos_2018_r/blob/master/s3_gbif_demo/Download_gbif.md)
-#source("./R/f_download_GBIF_API.R")
-#download_GBIF_API(download_key=download_key,destfile_name="./data/tmp.zip",n_try=5,Sys.sleep_duration=30)
+download_GBIF_API <- function(download_key,destfile_name,n_try,Sys.sleep_duration){
+  start_time <- Sys.time()
+  n_try_count <- 1
+  
+  download_url <- paste("http://api.gbif.org/v1/occurrence/download/request/",
+                        download_key[1],sep="")
+  
+  try_download <- try(download.file(url=download_url,destfile=destfile_name,
+                                    quiet=TRUE, mode="wb"),silent = TRUE)
+  
+  while (inherits(try_download, "try-error") & n_try_count < n_try) {   
+    Sys.sleep(Sys.sleep_duration)
+    n_try_count <- n_try_count+1
+    try_download <- try(download.file(url=download_url,destfile=destfile_name,
+                                      quiet=TRUE, mode="wb"),silent = TRUE)
+    print(paste("trying... Download link not ready. Time elapsed (min):",
+                round(as.numeric(paste(difftime(Sys.time(),start_time, units = "mins"))),2)))
+  }
+}
+download_GBIF_API(download_key=download_key,destfile_name=paste0(temp,"/tmp.zip"),n_try=5,Sys.sleep_duration=30)
 
 # read inn the occurrence data
 occ <- rio::import(unzip(paste0(temp,"/tmp.zip"),files="occurrence.txt"))
+if (!dir.exists(here::here("data"))){
+  dir.create(here::here("data"))
+}
 saveRDS(occ,here::here("data", "GBIF_download.rds"))
 unlink(temp)
 
