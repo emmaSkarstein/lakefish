2b5a6c425c9b6165fa43b6df5636cb85e1f39207
Emma Sofie Skarstein <emma@skarstein.no>
2019-10-23 12:57:29 +0200

    Generalizing download procedure

--- GBIF_download/Download.R
+++ GBIF_download/Download.R
@@ -26,31 +26,32 @@ options(gbif_user=rstudioapi::askForPassword("my gbif username"))
 options(gbif_email=rstudioapi::askForPassword("my registred gbif e-mail"))
 options(gbif_pwd=rstudioapi::askForPassword("my gbif password"))
 
-options(gbif_user = "lyderiversen")
-options(gbif_email = "lyder.iversen95@hotmail.com")
-options(gbif_pwd="Retsam144")
 
 #-------------------------------------------------------------------------------
 # Set search parameters and get download KEY
 #-------------------------------------------------------------------------------
 
+# Salmo trutta - Ørret
+# Esox lucius - Gjedde
+# Salmo salar - Laks
+# Salvelinus alpinus - Røye
+# Rutilus rutilus - Mort
+# Perca fluviatilis - Abbor
+# Cyprinus carpio - Karpe
+
 # Find a taxonkey - get list of gbif keys to filter download
-key <- name_suggest(q='Esox lucius', rank='species')$key[1] 
+key <- name_suggest(q='Salmo salar', rank='species')$key[1] 
 
-# Crate spatial filter 
 
-# manually create polygon using mapedit
-# polygon <- mapedit::drawFeatures(map=NULL)
-# polygon <- sf::st_geometry(polygon)
-# my_wkt <- sf::st_as_text(polygon)
-# #wicket::validate_wkt(my_wkt)
-# geom_param <- paste("geometry", "within", my_wkt)
-# # Send download request
 
-# spawn download request - carefull, there is a max limit on simultanious downloads 
+#-------------------------------------------------------------------------
+# Send download request
+#-------------------------------------------------------------------------
+
+# Spawn download request - carefull, there is a max limit on simultanious downloads 
 # per user allowed by the GBIF API. 
 download_key <- occ_download(
-  'taxonKey = 2346633',
+  paste('taxonKey = ', key),
   'hasCoordinate = TRUE',
   'country = NO',
   #geom_param,
@@ -58,17 +59,11 @@ download_key <- occ_download(
 ) %>% 
   occ_download_meta
 
-
-# Download data when ready (see https://www.gbif.org/user/download). 
+# For overview of your downloads, see https://www.gbif.org/user/download . 
 # The preparation will of the data may take some time.
 temp <- tempdir()
-# download.file(url=paste("http://api.gbif.org/v1/occurrence/download/request/",
-#                         download_key[1],sep=""),
-#               destfile=paste0(temp,"/tmp.zip"),
-#               quiet=TRUE, mode="wb")
 
-# or....
-# coffebreak version (see https://github.com/GBIF-Europe/nordic_oikos_2018_r/blob/master/s3_gbif_demo/Download_gbif.md)
+# Coffebreak version (see https://github.com/GBIF-Europe/nordic_oikos_2018_r/blob/master/s3_gbif_demo/Download_gbif.md)
 download_GBIF_API <- function(download_key,destfile_name,n_try,Sys.sleep_duration){
   print(paste("Attempting to download data from GBIF with", n_try,"attempts at", Sys.sleep_duration, "second intervals."))
   start_time <- Sys.time()
@@ -91,17 +86,26 @@ download_GBIF_API <- function(download_key,destfile_name,n_try,Sys.sleep_duratio
 }
 download_GBIF_API(download_key=download_key,destfile_name=paste0(temp,"/tmp.zip"),n_try=5,Sys.sleep_duration=30)
 print(paste("------IGNORE THE WARNING MESSAGES------"))
-# read inn the occurrence data
+
+
+#--------------------------------------------------------------------------
+# Read inn the occurrence data
+#--------------------------------------------------------------------------
+
 occ <- rio::import(unzip(paste0(temp,"/tmp.zip"),files="occurrence.txt"))
-if (!dir.exists(here::here("data"))){
+if (!dir.exists(here::here("data"))){ # Create "data"-folder if this doesn't exist
   dir.create(here::here("data"))
 }
-saveRDS(occ,here::here("data", "GBIF_download.rds"))
-unlink(temp)
 
+# Save the occurrence data to a file
+saveRDS(occ, here::here("data", paste0("GBIF_download_", key, ".rds")))
+
+unlink(temp)
+unlink(here::here("GBIF_download", "occurrence.txt"))
+
+
+# Write download key and citation to .rds files for reference (also available on https://www.gbif.org/user/download)
+saveRDS(download_key,here::here("data", paste0("GBIF_download_key_", key, ".rds")))
+        
 # CITE YOUR DATA!!! 
 citation <- paste0("GBIF Occurrence Download https://doi.org/", download_key[2], " accessed via GBIF.org on ", Sys.Date())
-
-# Write download key and citation to .rds files for refference (also awailable on https://www.gbif.org/user/download)
-saveRDS(download_key,here::here("data","GBIF_download_key.rds"))
-        
