98bad63a7fa5e9c6ad6a27a30b11278b37402ab2
Lyder Bøe Iversen <lyderi@ntnu.no>
2019-10-30 13:45:36 +0100

    Work in progress: creating plots of all species

--- R/testscript.R
+++ R/testscript.R
@@ -29,25 +29,25 @@ pick <- function(condition){
 norway1980 <- ggplot(data = occ_nona, aes(x = decimalLongitude, y = decimalLatitude)) +
   geom_map(data = norway, map = norway, aes(long, lat, map_id=region), color="#2b2b2b", fill="white") +
   geom_point(data = occ_matched, aes(x = decimalLongitude, y = decimalLatitude), size=0.001, color = "grey") +
-  geom_point(data = pick(year<1980 & year>1970), size=0.001, color = "red") +
+  geom_point(data = pick(year<1980 & year>=1970), size=0.001, color = "red") +
   ggtitle("Fish data between 1970 and 1980")
 
 norway1990 <- ggplot(data = occ_nona, aes(x = decimalLongitude, y = decimalLatitude)) +
   geom_map(data = norway, map = norway, aes(long, lat, map_id=region), color="#2b2b2b", fill="white") +
   geom_point(data = occ_matched, aes(x = decimalLongitude, y = decimalLatitude), size=0.001, color = "grey") +
-  geom_point(data = pick(year<1990 & year>1980), size=0.001, color = "red") +
+  geom_point(data = pick(year<1990 & year>=1980), size=0.001, color = "red") +
   ggtitle("Fish data between 1980 and 1990")
 
 norway2000 <- ggplot(data = occ_nona, aes(x = decimalLongitude, y = decimalLatitude)) +
   geom_map(data = norway, map = norway, aes(long, lat, map_id=region), color="#2b2b2b", fill="white") +
   geom_point(data = occ_matched, aes(x = decimalLongitude, y = decimalLatitude), size=0.001, color = "grey") +
-  geom_point(data = pick(year<2000 & year>1990), size=0.001, color = "red") +
+  geom_point(data = pick(year<2000 & year>=1990), size=0.001, color = "red") +
   ggtitle("Fish data between 1990 and 2000")
 
 norway2010 <- ggplot(data = occ_nona, aes(x = decimalLongitude, y = decimalLatitude)) +
   geom_map(data = norway, map = norway, aes(long, lat, map_id=region), color="#2b2b2b", fill="white") +
   geom_point(data = occ_matched, aes(x = decimalLongitude, y = decimalLatitude), size=0.001, color = "grey") +
-  geom_point(data = pick(year<2010 & year>2000), size=0.001, color = "red") +
+  geom_point(data = pick(year<2010 & year>=2000), size=0.001, color = "red") +
   ggtitle("Fish data between 2000 and 2010")
 
 plot_grid(norway1980, norway1990, norway2000, norway2010, labels = "AUTO", align = "h")
@@ -59,3 +59,13 @@ ggplot(data = occ_matched, aes(x = decimalLongitude, y = decimalLatitude, color 
   geom_point(data = pick(species == "Salvelinus alpinus"),size=0.001) +
   theme(legend.position = c(1,0), legend.justification = c(1,0)) +
   labs(color = "Species")
+
+for (i in species_table$species){
+  species_plot <- ggplot(data = occ_matched, aes(x = decimalLongitude, y = decimalLatitude, color = factor(species))) +
+    geom_map(data = norway, map = norway, aes(long, lat, map_id=region), color="#2b2b2b", fill="white") +
+    geom_point(data = occ_matched, aes(x = decimalLongitude, y = decimalLatitude), size=0.001, color = "grey") +
+    geom_point(data = pick(species == i),size=0.001) +
+    theme(legend.position = c(1,0), legend.justification = c(1,0)) +
+    labs(color = "Species")
+  ggsave(filename = paste0("plot", i, ".png"), plot = species_plot, path = here::here("data"),width=10,height = 10)
+}
