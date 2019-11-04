6d391be3e65e2ceb43e236a5bf591ca2455b27c8
Lyder Bøe Iversen <lyderi@ntnu.no>
2019-11-04 11:06:54 +0100

    added subplot titles

--- R/testscript.R
+++ R/testscript.R
@@ -10,7 +10,7 @@ library(maps)
 occ_matched <- readRDS(here::here("data","occ_matched_allfish.rds"))
 lakes <- readRDS(here::here("data","lake_polygons.rds"))
 
-species_table <- occ_matched %>% group_by(species) %>% summarize(count=n()) %>% filter(count > 1000) %>% arrange(desc(count))
+species_table <- occ_matched %>% group_by(species) %>% summarize(count=n()) %>% filter(count > 1000) %>% arrange(desc(count)) %>% top_n(n = 12, wt = count)
 occ_matched <- occ_matched %>% filter(occ_matched$species %in% species_table$species)
 
 occ_nona <- occ_matched[complete.cases(occ_matched$year,occ_matched$month,occ_matched$day),]
@@ -19,7 +19,7 @@ norway <- ggplot2::map_data("world", region = "Norway(?!:Svalbard)")
 ggplot() +
   geom_map(data = norway, map = norway, aes(long, lat, map_id=region), color="#2b2b2b", fill=NA) +
   geom_point(aes(x = lakes$decimalLongitude, y = lakes$decimalLatitude), size=0.001, color = "grey") +
-  geom_point(aes(x = occ_matched$decimalLongitude, y = occ_matched$decimalLatitude), size=0.001, color = "red")
+  geom_point(aes(x = occ_matched$decimalLongitude, y = occ_matched$decimalLatitude, color = factor(occ_matched$species)), size=0.001)
 
 library(cowplot)
 pick <- function(condition){
@@ -51,7 +51,7 @@ norway2010 <- ggplot(data = occ_nona, aes(x = decimalLongitude, y = decimalLatit
   ggtitle("Fish data between 2000 and 2010")
 
 plot_grid(norway1980, norway1990, norway2000, norway2010, labels = "AUTO", align = "h")
-
+########################
 ggplot(data = occ_matched, aes(x = decimalLongitude, y = decimalLatitude, color = factor(species))) +
   geom_map(data = norway, map = norway, aes(long, lat, map_id=region), color="#2b2b2b", fill="white") +
   geom_point(data = occ_matched, aes(x = decimalLongitude, y = decimalLatitude), size=0.001, color = "grey") +
@@ -59,7 +59,13 @@ ggplot(data = occ_matched, aes(x = decimalLongitude, y = decimalLatitude, color 
   geom_point(data = pick(species == "Salvelinus alpinus"),size=0.001) +
   theme(legend.position = c(1,0), legend.justification = c(1,0)) +
   labs(color = "Species")
+########################
+ggplot(data = occ_matched, aes(x = decimalLongitude, y = decimalLatitude)) +
+  geom_map(data = norway, map = norway, aes(long, lat, map_id=region), color="#2b2b2b", fill="white") +
+  geom_point(size=0.001, color = "red3") +
+  facet_wrap(~species)
 
+########################
 for (i in species_table$species){
   species_plot <- ggplot(data = occ_matched, aes(x = decimalLongitude, y = decimalLatitude, color = factor(species))) +
     geom_map(data = norway, map = norway, aes(long, lat, map_id=region), color="#2b2b2b", fill="white") +
